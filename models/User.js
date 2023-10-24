24c44385051c22329f7c7ce201a25524e53d499a
Muhammad Jazzel Mehmood <jazzelmehmood4@gmail.com>
2023-10-24 21:31:11 +0500

    Added User schema

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
